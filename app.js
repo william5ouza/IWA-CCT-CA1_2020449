429379e4ba6b5d85ecf28309c8fac863f06c11cd
William5ouza <will.dasilva.souza@gmail.com>
2020-12-29 22:30:52 +0000

    New page add Open image from catalog

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ router.get('/get/html', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'}); //We are responding to the client that the content served back is HTML and the it exists (code 200)
 
     var xml = fs.readFileSync('BrJewelDublin.xml', 'utf8'); //We are reading in the XML file
-    var xsl = fs.readFileSync('BrJewelDublin.xsl', 'utf8'); //We are reading in the XSL file
+    var xsl = fs.readFileSync('BrJewelDublin.xml', 'utf8'); //We are reading in the XSL file
 
     var doc = xmlParse(xml); //Parsing our XML file
     var stylesheet = xmlParse(xsl); //Parsing our XSL file
--- views/js/loading.js
+++ views/js/loading.js
@@ -1,17 +1,27 @@
-let url = "catalog.xml";
-$.ajax(url)
-.done(function(xml){
-    $(xml).find("entree").each(function(){
-    $("#jewels").append(`<div class="jewel">
-                         
-                        <image src="${ $(this).find("image").text()}" class="image-jewel">
+$ (function(){
+     $.ajax({
+      type: "GET",
+      url: "catalog.xml", // replace with absolute URL of your gallery's xml file
+      dataType: "xml",
+      success: function(xml) {
+    $(xml).find("necklaces").each(function(){
+    $("#Necklace").append(` <div class="jewel">
+                        
+                        <image src="${ $(this).find("image").text()}" width="350" height=250">
                         <p class="item">${ $(this).find("item").text()}</p>
                         <p class="price">${ $(this).find("price").text()}</p>
                         </div>
+                        </div>`);
+    })
+    $(xml).find("entree").each(function(){
+    $("#Necklace").append(` <div class="jewel">
                         
-                        `);
-     });                    
-})
-.fail(function(){
-    alert("Xml file could not be read");
-})
+                        <image src="${ $(this).find("image").text()}" width="350" height=250">
+                        <p class="item">${ $(this).find("item").text()}</p>
+                        <p class="price">${ $(this).find("price").text()}</p>
+                        </div>
+                        </div>`);
+    });
+      }
+    });
+  });
